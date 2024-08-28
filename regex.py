f3379062f1534c11e9b081b303d9af679f3366e7
austin-area61 <austinonyango.area61@gmail.com>
2024-08-28 07:04:49 +0300

    Modified regex.py

--- regex.py
+++ regex.py
@@ -1,2 +1,4 @@
 import re
 phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumberRegex.search('Mi numero de telefono es 415-666-4343.')
+print('Phone number found: ' +mo.group())
